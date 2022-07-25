36ddf5499361b22c62812890e0897f53ca6f3f51
haile17 <hailefikere17@gmail.com>
2022-07-25 01:25:57 -0700

    sorting alorithms

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,66 +1,44 @@
 #include "sort.h"
-#include "stdlib.h"
-
 /**
- * counting_sort - sorts an array of integers in ascending order using the
- * Counting sort algorithm
- * @array: array to sort
- * @size: size of the array to sort
- *
- * Return: void
+ * counting_sort - sorts an array of integers in ascending
+ * order using the Counting sort algorithm
+ * @array: pointer to array
+ * @size: size of the array
  */
 void counting_sort(int *array, size_t size)
 {
-	int i, max;
-	int *count = NULL, *copy = NULL;
-	size_t j, temp, total = 0;
+	int n, j, *count_array, *aux;
+	size_t i;
 
+	if (!array || size < 2)
+		return;
+	n = array[0];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > n)
+			n = array[i];
+	}
+	count_array = calloc((n + 1), sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		count_array[array[i]]++;
+	}
+	for (j = 1; j < n; j++)
+	{
+		count_array[j + 1] += count_array[j];
+	}
+	print_array(count_array, n + 1);
 
-	if (array == NULL || size < 2)
-		return;
-	copy = malloc(sizeof(int) * size);
-	if (copy == NULL)
-		return;
-	for (j = 0, max = 0; j < size; j++)
+	aux = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
 	{
-		copy[j] = array[j];
-		if (array[j] > max)
-			max = array[j];
+		count_array[array[i]]--;
+		aux[count_array[array[i]]] = array[i];
 	}
-	count = malloc(sizeof(int) * (max + 1));
-	if (count == NULL)
+	for (i = 0; i < size; i++)
 	{
-		free(copy);
-		return;
+		array[i] = aux[i];
 	}
-	for (i = 0; i <= max; i++)
-		count[i] = 0;
-	for (j = 0; j < size; j++)
-		count[array[j]] += 1;
-	for (i = 0; i <= max; i++)
-	{
-		temp = count[i];
-		count[i] = total;
-		total += temp;
-	}
-	for (j = 0; j < size; j++)
-	{
-		array[count[copy[j]]] = copy[j];
-		count[copy[j]] += 1;
-	}
-	print_array(count, max + 1);
-	free(count);
-	free(copy);
-}
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    counting_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	free(aux);
+	free(count_array);
 }
